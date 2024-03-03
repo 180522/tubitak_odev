bc534e89b47be6173ee45732232198f28fad553b
180522 <104648457+180522@users.noreply.github.com>
2024-03-03 23:28:44 +0300

    tubitak_proje.py

--- tubitak_proje.py
+++ tubitak_proje.py
@@ -22,17 +22,100 @@ n=input("Kaçıncı fibonaci sayısını istiyorsunuz= ")
 n=int(n)
 m=int(n+1)
 o=int(n+2)
-k=int(n+3)
 a=fibonacci(n)
 b=fibonacci(m)
 c=fibonacci(o)
-d=fibonacci(k)
-e=(2*b-a)/(2*b+a)
-w=(2*d-c)/(2*d+c)
-e=Decimal((2*b-a)/(2*b+a))
-f=Decimal((2*d-c)/(2*d+c))
-print(e)
-print(f)
+k=(2*b-a)/(2*b+a)
+l=(1-k**2)**(1/2)
+p=k/l
+r=1/p
+x=(2*c-b)/(2*c+b)
+y=(1-x**2)**(1/2)
+z=x/y
+t=1/z
+xc=n%16
+if xc==0:
+    e=Decimal(k)
+    f=Decimal(x)
+    print(e)
+    print(f)
+elif xc==1:
+    
+    e=Decimal(k)
+    f=Decimal(y)
+    print(e)
+    print(f)
+elif xc==2:
+    e=Decimal(k)
+    f=Decimal(z)
+    print(e)
+    print(f)
+elif xc==3:
+    e=Decimal(k)
+    f=Decimal(t)
+    print(e)
+    print(f)
+elif xc==4:
+    e=Decimal(l)
+    f=Decimal(x)
+    print(e)
+    print(f)
+elif xc==5:
+    e=Decimal(l)
+    f=Decimal(y)
+    print(e)
+    print(f)
+elif xc==6:
+    e=Decimal(l)
+    f=Decimal(z)
+    print(e)
+    print(f)
+elif xc==7:
+    e=Decimal(l)
+    f=Decimal(t)
+    print(e)
+    print(f)
+elif xc==8:
+    e=Decimal(p)
+    f=Decimal(x)
+    print(e)
+    print(f)
+elif xc==9:
+    e=Decimal(p)
+    f=Decimal(y)
+    print(e)
+    print(f)
+elif xc==10:
+    e=Decimal(p)
+    f=Decimal(z)
+    print(e)
+    print(f)
+elif xc==11:
+    e=Decimal(p)
+    f=Decimal(t)
+    print(e)
+    print(f)
+elif xc==12:
+    e=Decimal(r)
+    f=Decimal(x)
+    print(e)
+    print(f)
+elif xc==13:
+    e=Decimal(r)
+    f=Decimal(y)
+    print(e)
+    print(f)
+elif xc==14:
+    e=Decimal(r)
+    f=Decimal(z)
+    print(e)
+    print(f)
+elif xc==15:
+    e=Decimal(r)
+    f=Decimal(t)
+    print(e)
+    print(f)
+
 e=str(e)
 f=str(f)
 g=e
@@ -95,7 +178,7 @@ sayi=[]
 sayi2=[]
 esitsizlik=False
 for i in range(len(e)):
-    if i<=42 and esitsizlik:
+    if i<=42 and esitsizlik and i>0:
         j+=g[i]
         j+=h[i]
         de=int(j)
@@ -103,8 +186,8 @@ for i in range(len(e)):
 		#print(de)
         sayi.append(de)
 		#print(len(sayi))
-        sayi2.append(sayi[::-1])
-    elif i<=42 and g[i]!=h[i]:
+        sayi2.append(sayi[:1:-1])
+    elif i<=42 and g[i]!=h[i] and i>0:
         j+=g[i]
         j+=h[i]
         de=int(j)
@@ -112,7 +195,7 @@ for i in range(len(e)):
 		#print(de)
         sayi.append(de)
 		#print(len(sayi))
-        sayi2.append(sayi[::-1])
+        sayi2.append(sayi[:1:-1])
         esitsizlik=True
 son=sayi2[len(sayi)-1][0]
 print(son)
